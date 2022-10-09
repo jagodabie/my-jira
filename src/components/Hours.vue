a92392329c2f2a6b95b73eeffcfeb8fa943effb0
jagodabie <51316252+jagodabie@users.noreply.github.com>
2022-10-09 10:57:53 +0200

    feat: global var, week refactor

--- src/components/Hours.vue
+++ src/components/Hours.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hours">
-    <div class="hour" v-for="(hour, index) in hours" :key="index">
-      {{ hour }}
+    <div class="hour-row" v-for="(hour, index) in hours" :key="index">
+      <div class="hour-container">{{ hour }}</div>
+      <div class="day-of-week__hour"></div>
     </div>
   </div>
 </template>
@@ -14,10 +15,30 @@ export default class Hours extends Vue {
   hours!: number[];
 }
 </script>
-<style scoped>
-.hour {
-  font-size: 2px;
+<style lang="scss">
+.hours {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.hour-row {
+  display: flex;
+  border-left: $border-gray44;
+  border-right: $border-gray44;
+  border-top: $border-gray44;
+}
+.hour-container {
+  font-size: 20px;
   padding: 20px;
-  border-bottom: 0.1px solid rgb(175, 168, 168);
+  border-right: $border-gray44;
+  text-align: left;
+  box-sizing: border-box;
+  line-height: 1;
+  width: calc($day-of-week-width);
+}
+
+.day-of-week__hour {
+  width: auto;
+  width: 80%;
 }
 </style>
